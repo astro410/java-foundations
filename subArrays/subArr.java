a2ce096e1001060a9e61153b70d8fb690d808dbd
brinda410 <brindarswamy@gmail.com>
2026-05-23 12:36:55 +0530

    Prints subarrays

---
+++ subArrays/subArr.java
@@ -0,0 +1,19 @@
+package subArrays;
+
+public class subArr {
+    public static void subArrays(int numbers[]){
+        for(int i=0;i<numbers.length;i++){
+            int start=i;
+            for(int j=0;j<numbers.length;j++){
+                int end=j;
+                for(int k=start;k<=end;k++){
+                    System.out.print(numbers[k]);
+                }System.out.println();
+            }
+        }
+    }
+    public static void main(String[]args){
+        int numbers[]={2,4,6,8,10};
+        subArrays(numbers);
+    }
+}
